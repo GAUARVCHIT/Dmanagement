bbcc28f4c357d19bbacb1a3a9e20e0e084f2cd4b
Utkarsh <37050056+utkarsh530@users.noreply.github.com>
2018-10-26 20:39:39 +0530

    Attached MainActivity with ImageButton

--- app/src/main/java/com/example/gaurav/dusgsapiextraction/HomeActivity.java
+++ app/src/main/java/com/example/gaurav/dusgsapiextraction/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.gaurav.dusgsapiextraction;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageButton equake= (ImageButton) findViewById(R.id.imageButton2);
+        equake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
